8c5952b73137b97e18269d88533ea4f47af40fa1
Danield <1762169697@qq.com>
2018-01-30 11:31:39 +1300

    change
    change

--- startproject.php
+++ startproject.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" href="images/fav.png"/>
     <link rel="stylesheet" href="css/bundle.css">
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
@@ -67,22 +68,22 @@
           <!-- Navigation Menu-->
           <ul class="navigation-menu">
             <li>
-              <a href="#">
+              <a href="work.html">
                 Work
               </a>
             </li>
             <li>
-              <a href="#" onclick="aboutFun()">
+              <a href="about.html" onclick="aboutFun()">
                 About
               </a>
             </li>
             <li>
-              <a href="#">
+              <a href="careers.html">
                 Careers
               </a>
             </li>
             <li>
-              <a href="#">
+              <a href="contact.html">
                 Contact
               </a>
             </li>
